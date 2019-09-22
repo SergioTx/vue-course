45a6603bea149032b88c3fcc3e8a0c2280e6fe78
SergioTx <txitxo37@gmail.com>
2019-09-22 17:48:37 +0200

    132

--- app3/src/components/New.vue
+++ app3/src/components/New.vue
@@ -15,6 +15,12 @@ export default {
   },
   destroyed() {
     console.log("Destroyed!");
+  },
+  activated() {
+    console.log("Activated!");
+  },
+  deactivated() {
+    console.log("Deactivated!");
   }
 };
 </script>
