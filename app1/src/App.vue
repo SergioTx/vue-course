b57636e41c3c9fd00a91a429b82fb970672442e6
SergioTx <txitxo37@gmail.com>
2019-09-22 15:59:55 +0200

    95

--- app1/src/App.vue
+++ app1/src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <h1>Hello World</h1>
+  <p>Server Status: {{ status }}</p>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      status: "Critical"
+    };
+  }
+};
 </script>
 
 <style>
