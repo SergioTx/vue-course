f71acfeb0106c09c3ae56d786a288144b8a66828
SergioTx <txitxo37@gmail.com>
2019-09-22 16:31:42 +0200

    106

--- app2/src/components/UserDetail.vue
+++ app2/src/components/UserDetail.vue
@@ -2,10 +2,12 @@
   <div class="component">
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
+    <p>User Name: {{ name }}</p>
   </div>
 </template>
 
 <script>
+export default {};
 </script>
 
 <style scoped>
