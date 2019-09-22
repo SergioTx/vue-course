612a1006ba5729996b925b4b9d6efb1a24e041a8
SergioTx <txitxo37@gmail.com>
2019-09-22 23:16:15 +0200

    158

--- app5/src/App.vue
+++ app5/src/App.vue
@@ -71,12 +71,12 @@
       <hr />
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-          <button class="btn btn-primary">Submit!</button>
+          <button class="btn btn-primary" @click.prevent="submitted">Submit!</button>
         </div>
       </div>
     </form>
     <hr />
-    <div class="row">
+    <div class="row" v-if="isSubmitted">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <div class="panel panel-default">
           <div class="panel-heading">
@@ -119,8 +119,14 @@ export default {
       gender: "Male",
       priorities: ["High", "Medium", "Low"],
       selectedPriority: "High",
-      dataSwitch: true
+      dataSwitch: true,
+      isSubmitted: false
     };
+  },
+  methods: {
+    submitted() {
+      this.isSubmitted = true;
+    }
   },
   components: {
     appSwitch: Switch
