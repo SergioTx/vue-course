9800f0b80b2fd00ef88c57276198319863321c56
SergioTx <txitxo37@gmail.com>
2019-09-22 22:54:40 +0200

    150

--- app5/src/App.vue
+++ app5/src/App.vue
@@ -7,15 +7,15 @@
           <hr />
           <div class="form-group">
             <label for="email">Mail</label>
-            <input type="text" id="email" class="form-control" v-model="email" />
+            <input type="text" id="email" class="form-control" v-model="userData.email" />
           </div>
           <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" id="password" class="form-control" />
+            <input type="password" id="password" class="form-control" v-model="userData.password" />
           </div>
           <div class="form-group">
             <label for="age">Age</label>
-            <input type="number" id="age" class="form-control" />
+            <input type="number" id="age" class="form-control" v-model="userData.age" />
           </div>
         </div>
       </div>
@@ -72,9 +72,9 @@
             <h4>Your Data</h4>
           </div>
           <div class="panel-body">
-            <p>Mail: {{ email }}</p>
-            <p>Password:</p>
-            <p>Age:</p>
+            <p>Mail: {{ userData.email }}</p>
+            <p>Password: {{ userData.password }}</p>
+            <p>Age: {{ userData.age }}</p>
             <p>Message:</p>
             <p>
               <strong>Send Mail?</strong>
@@ -96,7 +96,11 @@
 export default {
   data() {
     return {
-      email: ""
+      userData: {
+        email: "",
+        password: "",
+        age: 27
+      }
     };
   }
 };
