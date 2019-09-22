b4e22f00f867d8a02b267fe211db665443d0f2ba
SergioTx <txitxo37@gmail.com>
2019-09-22 16:08:17 +0200

    96

---
+++ app1/src/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Server Stratus: {{ status }}</p>
+    <hr />
+    <button @click="changeStatus">Change</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      status: 'Critical'
+    };
+  },
+  methods: {
+    changeStatus() {
+      this.status = 'Normal';
+    }
+  }
+};
+</script>
