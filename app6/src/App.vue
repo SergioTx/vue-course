99d90f4ecb1e17856fc46acb726120606e3c4aa2
SergioTx <txitxo37@gmail.com>
2019-09-23 20:16:18 +0200

    172

--- app6/src/App.vue
+++ app6/src/App.vue
@@ -12,7 +12,9 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Custom Directives</h1>
         <p v-highlight:background.delayed="'red'">Color this</p>
-        <p v-local-highlight:background.delayed.blink="'red'">Color this</p>
+        <p
+          v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 300}"
+        >Color this</p>
       </div>
     </div>
   </div>
@@ -28,8 +30,8 @@ export default {
           delay = 3000;
         }
         if (binding.modifiers["blink"]) {
-          let mainColor = binding.value;
-          let secondColor = "blue";
+          let mainColor = binding.value.mainColor;
+          let secondColor = binding.value.secondColor;
           let currentColor = mainColor;
           setInterval(() => {
             currentColor =
@@ -41,13 +43,13 @@ export default {
                 el.style.color = currentColor;
               }
             }, delay);
-          }, 1000);
+          }, binding.value.delay);
         } else {
           setTimeout(() => {
             if (binding.arg == "background") {
-              el.style.backgroundColor = binding.value;
+              el.style.backgroundColor = binding.value.mainColor;
             } else {
-              el.style.color = binding.value;
+              el.style.color = binding.value.mainColor;
             }
           }, delay);
         }
