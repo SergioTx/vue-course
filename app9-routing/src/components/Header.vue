0ae1148f97314cf147c4a14d7e2a4d9da50b4e6e
SergioTx <txitxo37@gmail.com>
2019-09-25 19:35:48 +0200

    235

---
+++ app9-routing/src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <ul class="nav nav-pills">
+    <li>
+      <router-link to="/">Home</router-link>
+    </li>
+    <li>
+      <router-link to="/user">User</router-link>
+    </li>
+  </ul>
+</template>
