2149606a6abf53df1e56cdb18e04007f6e530fe8
SergioTx <txitxo37@gmail.com>
2019-09-25 19:28:17 +0200

    233

--- app9-routing/src/main.js
+++ app9-routing/src/main.js
@@ -1,7 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import {
+  routes
+} from './routes';
+
+const router = new VueRouter({
+  routes
+});
+
+Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
